76988a77fcb4282d4f7625ed3267ac805584955e
andreasthuis <andreasdeborger27@gmail.com>
2026-07-22 16:40:57 +0200

    Initial commit

---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,14 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        
+      </div>
+    </section>
+  );
+}
