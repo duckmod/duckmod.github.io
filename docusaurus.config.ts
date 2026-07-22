8976f48ab8707a862a38d2b57ce14d6fc0e20f6b
andreasthuis <andreasdeborger27@gmail.com>
2026-07-22 17:10:10 +0200

    fix my mistake :sob:

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'duckmod', // Usually your GitHub org/user name.
-  projectName: 'duckloader', // Usually your repo name.
+  projectName: 'duckmod.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
